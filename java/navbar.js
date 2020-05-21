2cfa555b11b416f481c2d4d0f6fc1319815e607a
giusycinotti95 <63308656+giusycinotti95@users.noreply.github.com>
2020-05-21 19:43:41 +0200

    migliorata la homepage con slideshow ecc

---
+++ java/navbar.js
@@ -0,0 +1,8 @@
+function toggle_navbar() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
