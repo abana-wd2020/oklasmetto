af8d34b1947ec861f01670ec76811d3cc98bffd4
giusycinotti95 <63308656+giusycinotti95@users.noreply.github.com>
2020-05-31 18:29:01 +0200

    ULTIMIIII AGGIORNAMENTIIIIII

--- java/navbar.js
+++ java/navbar.js
@@ -1,9 +1,20 @@
 function openNav() {
   document.getElementById("mob_nav").style.width = "100%";
-  document.getElementById("nav_button").style.color = "white";
+  document.getElementById("nav_button").style.visibility = "hidden";
 }
 
 function closeNav() {
   document.getElementById("mob_nav").style.width = "0%";
-  document.getElementById("nav_button").style.color = "black";
+  document.getElementById("nav_button").style.visibility = "visible";
+}
+
+
+function openSearch() {
+  document.getElementById("search_menu").style.width = "100%";
+  document.getElementById("search_icon").style.visibility = "hidden";
+}
+
+function closeSearch() {
+  document.getElementById("search_menu").style.width = "0%";
+  document.getElementById("search_icon").style.visibility = "visible";
 }
